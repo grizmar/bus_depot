d6a77b6f9a317fcfa74a6b304b02ed60e5981a95
grizmar <grizmar5@gmail.com>
2020-04-19 20:24:10 +0300

    Добавлены миграции

--- config/db.php
+++ config/db.php
@@ -1,14 +1,9 @@
 <?php
 
 return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
-
-    // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
+    'class'    => 'yii\db\Connection',
+    'dsn'      => 'mysql:host=mysql;dbname=test',
+    'username' => 'test',
+    'password' => 'test',
+    'charset'  => 'utf8',
 ];
